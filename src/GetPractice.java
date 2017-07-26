de7b545bf7804927469c3491a54e57143214f1e4
adfoy <adfoy@FOYLAPTOP>
2017-07-26 14:10:35 -0600

    Bug fix

--- src/GetPractice.java
+++ src/GetPractice.java
@@ -5,6 +5,7 @@ public class GetPractice {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Yeah Baby!");
+		System.out.println("Bug fix 1234");
 	}
 
 }
