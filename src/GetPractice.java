e9a9dab481e5d91558b0aff43cee51091b64fc2e
adfoy <adfoy@FOYLAPTOP>
2017-07-26 14:03:12 -0600

    Next change

--- src/GetPractice.java
+++ src/GetPractice.java
@@ -3,7 +3,8 @@ public class GetPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
+		System.out.println("Hello World!");
+		System.out.println("Yeah Baby!");
 	}
 
 }
